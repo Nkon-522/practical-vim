fb6465d42f3769f312c7790c75bf15061d62428f
Nkon-522 <nkon.cs@gmail.com>
2022-07-26 23:38:44 -0500

    Files added

---
+++ 1-The-Vim-Way/2_foo_bar.js
@@ -0,0 +1,7 @@
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
+
+
+// Command A goes to end of current line and enters insert mode.
+// Command $ goes to end of current line 
---
+++ 1-The-Vim-Way/3_concat.js
@@ -0,0 +1,5 @@
+var foo = "method(" + argument1 + "," + argument2 + ")";
+
+// Command f-{char} moves cursor to next char.
+// Command ; repeats last search.
+// Command s deletes current character and enters insert mode.
